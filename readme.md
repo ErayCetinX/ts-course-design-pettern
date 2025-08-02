1c792fc0545bb4ce72dcee6fab0ba3273b2bc46a
ErayCetinX <cetin.eray@hotmail.com>
2025-08-03 00:04:00 +0300

    readme file is added to project

---
+++ readme.md
@@ -0,0 +1,12 @@
+#### This project was done in Stephen Grid's udemy [course](https://udemy.com/course/typescript-the-complete-developers-guide/)
+
+# How to use this repository
+
+### Clone repository
+`https://github.com/ErayCetinX/ts-course-design-pettern.git`
+
+### Install dependencies
+`npm install`
+
+### Run project
+`npm start`
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    let swap;
-    for (let i = 0; i < this.collection.length; i++) {
-      // instanceof, değerin o sınıfa ait olup olmadığını belirtir true/false döner
-      // instanceof, primitive tpye olmayan değerlerin tiplerini kontrol etmek için kullanılır
-      // Biz burada 'this.collection' ögesinin bir Array
-      // class'ına mı onu kontrol ettik
-      //
-      // Eğerki birden fazla sınıfı kontrol ediyorsak
-      // Bu yöntem 'instanceof' kötü bir yöntemdir
-      // Çünkü her değer için ayrı bir if statement olacaktır.
-      if (this.collection instanceof Array) {
-        swap = false;
-        for (let j = 0; j < this.collection.length - i - 1; j++) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            [this.collection[j], this.collection[j + 1]] = [this.collection[j + 1], this.collection[j]];
-            swap = true;
-          }
-        }
-        if (!swap) break;
-      }
-      // typeof primitive('number', 'string', 'boolean')
-      // değerlerini kontrol etmek için kullanıyor
-      if (typeof this.collection === "string") {
-         
-      }
-    }
-  }
-}
-
 const sorter = new Sorter([3, 2, 1, 56, 12, 33, 44]);
 sorter.sort()
 console.log(sorter.collection)
